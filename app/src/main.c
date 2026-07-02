8392db3df719dbe03c4f34f5d02a064876316278
ham22-tester <148444409+ham22-tester@users.noreply.github.com>
2026-07-02 22:39:34 +0900

    Update main.c

--- app/src/main.c
+++ app/src/main.c
@@ -1,1 +1,14 @@
+#include <zephyr/kernel.h>
+#include <zephyr/sys/printk.h>
 
+int main(void)
+{
+    printk("TCA9555 Test Start!\n");
+
+    while (1) {
+        printk("Running...\n");
+        k_sleep(K_SECONDS(1));
+    }
+
+    return 0;
+}
